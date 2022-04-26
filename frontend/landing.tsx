f832ec83f8c710d6628a9077e44cf27257dc5c3a
frankstew <frankjstew@gmail.com>
2022-04-25 23:29:12 -0700

    add circular enter button to landing page

--- frontend/landing.tsx
+++ frontend/landing.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import styles from './landing.module.css';
 
-
-
 export function LandingPage() {
   return (
-    <div className={styles.landingTitle}>
-      <h1 className={styles.titleText}>
-        I hope this website is irritating.
-      </h1>
-      <Link to="/achievements">Achievements page</Link>
+    <div>
+      <div className={styles.landingTitle}>
+        <h1 className={styles.titleText}>
+          This website is irritating.
+        </h1>
+      </div>
+      <div className={styles.enterSite}>
+        <Link to="/achievements">
+          <button className={styles.enterSiteButton}>
+            Enter
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
-
-//module.exports = {landingPage}
 
 
